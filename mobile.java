7c2e21cb39196ea34a52f676fd01fb26f351dc5f
Ivan <sevrep@yahoo.com>
2020-08-29 05:53:34 +0800

    Modified the mobile.java inside the first branch

--- mobile.java
+++ mobile.java
@@ -1,6 +1,7 @@
 class mobile {
 
-	public static void main(String[]args){
-		System.out.println("Hello world.");	
+	public static void main(String[] args){
+		System.out.println("Hello world.");
+		System.out.println("Hi world.");
 	}
 }
